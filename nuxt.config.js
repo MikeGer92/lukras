24ce5dbebf8d65b8adb6127a5945264e5cb1ca22
unknown <mikeman@inbox.ru>
2022-09-11 21:39:46 +0300

    added swiper-template

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/base.scss',
-    '@/assets/normalize.scss'
+    '@/assets/normalize.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -43,5 +43,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    standalone: true
   }
 };
