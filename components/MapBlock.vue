321c98835d70c8d0cd377e4487d09dfc10dbb3e2
unknown <mikeman@inbox.ru>
2022-09-11 13:08:06 +0300

    added ContactForm

--- components/MapBlock.vue
+++ components/MapBlock.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="map">
-
-
+    <ContactForm></ContactForm>
   </section>
 </template>
 
 <script>
+import ContactForm from './ContactForm.vue';
   export default {
-
-  }
+    components: { ContactForm }
+}
 </script>
 
 <style lang="scss">
@@ -16,6 +16,7 @@
     width: 100%;
     height: 800px;
     background: url('@/assets/images/map.png') 0px 0px/cover no-repeat;
+    padding: 136px  0  0 140px;
   }
 
 </style>
