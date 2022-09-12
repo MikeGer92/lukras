1734fd51cce4eb324acdf93cb74061f55f767f69
unknown <mikeman@inbox.ru>
2022-09-12 13:36:02 +0300

    added swiper for WorksGalary

--- components/OurGalary.vue
+++ components/OurGalary.vue
@@ -41,50 +41,6 @@ import Swiper from './Swiper.vue';
     text-transform: uppercase;
     color: #000000;
   }
-  &__images {
-    width: 100%;
-    max-width: 1600px;
-    margin: 0 auto;
-    height: 520px;
-    background: url('@/assets/images/galary.png') 0px 0px/cover no-repeat;
-  }
-  &__manage {
-    display: flex;
-    width: 100%;
-    max-width: 1600px;
-    margin: 0 auto;
-    border-bottom: 1px solid #E3E3E3;
-    &_wrapper {
-      width: 100%;
-      max-width: 1320px;
-      margin: 0 auto;
-      border-left: 1px solid #E3E3E3;
-      border-right: 1px solid #E3E3E3;
-      display: flex;
-      justify-content: space-between;
-    }
-    &_photo {
-      display: flex;
-      font-weight: 400;
-      font-size: 16px;
-      line-height: 18px;
-      text-align: center;
-      color: #000000;
-      &-title, &-num {
-        display: flex;
-        padding: 22px 40px 23px 40px;
-        border-right: 1px solid #E3E3E3;
-      }
-    }
-    &_btn {
-      display: flex;
-      &-prev, &-next {
-        padding: 24px;
-        border-left: 1px solid #E3E3E3;
-        cursor: pointer;
-      }
-    }
-  }
 }
 
 </style>
