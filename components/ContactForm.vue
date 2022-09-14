5d29cef72a641ece2ed36974a830ac22c7aa8abb
unknown <mikeman@inbox.ru>
2022-09-14 09:30:17 +0300

    added experimental ContactsForm 768px

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -67,6 +67,81 @@ import Social from './Social.vue';
 <style lang="scss">
   .small-form {
     display: none;
+    max-width: 100%;
+    border: 1px solid #E3E3E3;
+    background: #FFFFFF;
+    padding: 8px;
+    &__inter {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      border: 1px solid #E3E3E3;
+      &_title {
+        padding: 36px 32px;
+        font-family: 'Roboto Condensed';
+        font-weight: 700;
+        font-size: 32px;
+        line-height: 40px;
+        text-transform: uppercase;
+        color: #000000;
+        order: 0;
+      }
+      &_divider {
+        width: 100%;
+        height: 1px;
+        background: #E3E3E3;
+      }
+      &_wrapp {
+        padding: 36px 32px;
+        border-bottom: 1px solid #E3E3E3;
+        order: 2;
+      }
+      &_info {
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+        &-addr {
+          display: flex;
+          flex-direction: column;
+          &--title {
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 18px;
+            color: #3F3F3F;
+          }
+          &--address {
+            margin-top: 16px;
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 18px;
+            color: #000000;
+          }
+        }
+        &-cont {
+          display: flex;
+          column-gap: 72px;
+          &--phone, &--mail {
+            &_title {
+              font-weight: 400;
+              font-size: 16px;
+              line-height: 18px;
+              color: #3F3F3F;
+            }
+            &_num, &_addr {
+              margin-top: 16px;
+              font-weight: 500;
+              font-size: 16px;
+              line-height: 18px;
+              color: #000000;
+            }
+          }
+        }
+      }
+      &_social {
+        padding: 36px 32px;
+        order: 1;
+      }
+    }
   }
   .contact-form {
     max-width: 424px;
@@ -145,7 +220,11 @@ import Social from './Social.vue';
     }
   }
   @media (min-width: 390px) and (max-width: 768px) {
+    .small-form {
+      display: flex;
+    }
     .contact-form {
+      display: none;
       max-width: 724px;
       max-height: 280px;
       &__inter {
