64961bba744042aa15ec2a0de90cd66f89ed167f
unknown <mikeman@inbox.ru>
2022-09-14 09:57:05 +0300

    added ContactsForm 768px

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,57 +1,28 @@
 <template>
-  <div>
-    <div class="small-form">
-      <div class="small-form__inter">
-        <div class="small-form__inter_title">Наши контакты</div>
-        <div class="small-form__inter_divider"></div>
-        <div class="small-form__inter_wrapp">
-          <div class="small-form__inter_info">
-            <div class="small-form__inter_info-addr">
-              <div class="small-form__inter_info-addr--title">Адрес офиса</div>
-              <div class="small-form__inter_info-addr--address">г. Ростов-на-Дону, ул. Механизаторов 10</div>
+  <div class="contact-form">
+    <div class="contact-form__inter">
+      <div class="contact-form__inter_title">Наши контакты</div>
+      <div class="contact-form__inter_divider"></div>
+      <div class="contact-form__inter_wrapp">
+        <div class="contact-form__inter_info">
+          <div class="contact-form__inter_info-addr">
+            <div class="contact-form__inter_info-addr--title">Адрес офиса</div>
+            <div class="contact-form__inter_info-addr--address">г. Ростов-на-Дону, ул. Механизаторов 10</div>
+          </div>
+          <div class="contact-form__inter_info-cont">
+            <div class="contact-form__inter_info-cont--phone">
+              <div class="contact-form__inter_info-cont--phone_title">Телефон</div>
+              <div class="contact-form__inter_info-cont--phone_num">8 (800) 600-90-91</div>
             </div>
-            <div class="small-form__inter_info-cont">
-              <div class="small-form__inter_info-cont--phone">
-                <div class="small-form__inter_info-cont--phone_title">Телефон</div>
-                <div class="small-form__inter_info-cont--phone_num">8 (800) 600-90-91</div>
-              </div>
-              <div class="small-form__inter_info-cont--mail">
-                <div class="small-form__inter_info-cont--mail_title">E-mail</div>
-                <div class="small-form__inter_info-cont--mail_addr">info@lukras.ru</div>
-              </div>
+            <div class="contact-form__inter_info-cont--mail">
+              <div class="contact-form__inter_info-cont--mail_title">E-mail</div>
+              <div class="contact-form__inter_info-cont--mail_addr">info@lukras.ru</div>
             </div>
           </div>
-        </div>
-        <div class="small-form__inter_social">
-          <Social></Social>
         </div>
       </div>
-    </div>
-    <div class="contact-form">
-      <div class="contact-form__inter">
-        <div class="contact-form__inter_title">Наши контакты</div>
-        <div class="contact-form__inter_divider"></div>
-        <div class="contact-form__inter_wrapp">
-          <div class="contact-form__inter_info">
-            <div class="contact-form__inter_info-addr">
-              <div class="contact-form__inter_info-addr--title">Адрес офиса</div>
-              <div class="contact-form__inter_info-addr--address">г. Ростов-на-Дону, ул. Механизаторов 10</div>
-            </div>
-            <div class="contact-form__inter_info-cont">
-              <div class="contact-form__inter_info-cont--phone">
-                <div class="contact-form__inter_info-cont--phone_title">Телефон</div>
-                <div class="contact-form__inter_info-cont--phone_num">8 (800) 600-90-91</div>
-              </div>
-              <div class="contact-form__inter_info-cont--mail">
-                <div class="contact-form__inter_info-cont--mail_title">E-mail</div>
-                <div class="contact-form__inter_info-cont--mail_addr">info@lukras.ru</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="contact-form__inter_social">
-          <Social></Social>
-        </div>
+      <div class="contact-form__inter_social">
+        <Social></Social>
       </div>
     </div>
   </div>
@@ -67,81 +38,7 @@ import Social from './Social.vue';
 <style lang="scss">
   .small-form {
     display: none;
-    max-width: 100%;
-    border: 1px solid #E3E3E3;
-    background: #FFFFFF;
-    padding: 8px;
-    &__inter {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      border: 1px solid #E3E3E3;
-      &_title {
-        padding: 36px 32px;
-        font-family: 'Roboto Condensed';
-        font-weight: 700;
-        font-size: 32px;
-        line-height: 40px;
-        text-transform: uppercase;
-        color: #000000;
-        order: 0;
-      }
-      &_divider {
-        width: 100%;
-        height: 1px;
-        background: #E3E3E3;
-      }
-      &_wrapp {
-        padding: 36px 32px;
-        border-bottom: 1px solid #E3E3E3;
-        order: 2;
-      }
-      &_info {
-        display: flex;
-        flex-direction: column;
-        gap: 24px;
-        &-addr {
-          display: flex;
-          flex-direction: column;
-          &--title {
-            font-weight: 400;
-            font-size: 16px;
-            line-height: 18px;
-            color: #3F3F3F;
-          }
-          &--address {
-            margin-top: 16px;
-            font-weight: 500;
-            font-size: 16px;
-            line-height: 18px;
-            color: #000000;
-          }
-        }
-        &-cont {
-          display: flex;
-          column-gap: 72px;
-          &--phone, &--mail {
-            &_title {
-              font-weight: 400;
-              font-size: 16px;
-              line-height: 18px;
-              color: #3F3F3F;
-            }
-            &_num, &_addr {
-              margin-top: 16px;
-              font-weight: 500;
-              font-size: 16px;
-              line-height: 18px;
-              color: #000000;
-            }
-          }
-        }
-      }
-      &_social {
-        padding: 36px 32px;
-        order: 1;
-      }
-    }
+
   }
   .contact-form {
     max-width: 424px;
@@ -220,35 +117,38 @@ import Social from './Social.vue';
     }
   }
   @media (min-width: 390px) and (max-width: 768px) {
-    .small-form {
-      display: flex;
-    }
     .contact-form {
-      display: none;
-      max-width: 724px;
-      max-height: 280px;
+      display: flex;
+      max-width: 100%;
       &__inter {
         flex-direction: row;
+        justify-content: space-between;
         flex-wrap: wrap;
         &_title {
-          padding: 36px 32px 44px 32px;
+          width: 50%;
+          padding: 40px 32px;
+          order: 0;
+        }
+        &_divider {
+          display: none;
         }
         &_wrapp {
-          flex-direction: row;
-          padding: 40px 32px 32px 32px;
+          padding: 40px 32px;
+          border-top: 1px solid #E3E3E3;
+          border-bottom: none;
           order: 2;
         }
         &_info {
           width: 100%;
           flex-direction: row;
+          gap: 50px;
           &-cont {
-            gap: 32px;
+            column-gap: 48px;
           }
         }
         &_social {
+          padding: 36px 32px;
           order: 1;
-          padding: 36px 32px 44px 32px;
-          border-bottom: 1px solid #E3E3E3;
         }
       }
     }
