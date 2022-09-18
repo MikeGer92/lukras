c2269b7332ffeb7b3e708e10e749467afc108252
unknown <mikeman@inbox.ru>
2022-09-18 13:18:11 +0300

    deleted cursor-circle

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="main">
-    <div :class="[ 'g-cursor', { 'g-cursor_hover': hover }, {'g-cursor_hide': hideCursor} ]">
+    <!-- <div :class="[ 'g-cursor', { 'g-cursor_hover': hover }, {'g-cursor_hide': hideCursor} ]">
       <div :style="cursorCircle" class="g-cursor__circle"></div>
       <div class="g-cursor__point" ref="point" :style="cursorPoint"></div>
-    </div>
+    </div> -->
     <MainBlock></MainBlock>
     <transition name="calc"><CalcBlock v-if="getCalc"></CalcBlock></transition>
     <div class="calc" v-intersect="onCalcIntersect"></div>
@@ -64,22 +64,22 @@ export default {
       galaryShow: false,
       consultShow: false,
       mapShow: false,
-      xChild: 0,
-      yChild: 0,
-      xParent: 0,
-      yParent: 0,
-      hover: false,
-      hideCursor: true
+      // xChild: 0,
+      // yChild: 0,
+      // xParent: 0,
+      // yParent: 0,
+      // hover: false,
+      // hideCursor: true
 
     }
   },
   computed: {
-    cursorCircle() {
-      return `transform: translateX(${this.xParent}px) translateY(${this.yParent}px) translateZ(0) translate3d(0, 0, 0);`
-    },
-    cursorPoint() {
-      return `transform: translateX(${this.xChild - 3}px) translateY(${this.yChild - 3}px) translateZ(0) translate3d(0, 0, 0);`
-    },
+    // cursorCircle() {
+    //   return `transform: translateX(${this.xParent}px) translateY(${this.yParent}px) translateZ(0) translate3d(0, 0, 0);`
+    // },
+    // cursorPoint() {
+    //   return `transform: translateX(${this.xChild - 3}px) translateY(${this.yChild - 3}px) translateZ(0) translate3d(0, 0, 0);`
+    // },
     getCalc() {
       return this.calcShow
     },
@@ -110,40 +110,38 @@ export default {
     getMap() {
       return this.calcShow
     },
-
   },
   mounted() {
     // window.addEventListener('mousemove',this.mouseIsMoving)
-    document.addEventListener("mousemove", this.moveCursor);
-    document.addEventListener('mouseleave', e => {
-      this.hideCursor = true;
-    });
-    document.addEventListener('mouseenter', e => {
-      this.hideCursor = false;
-    });
+  //   document.addEventListener("mousemove", this.moveCursor);
+  //   document.addEventListener('mouseleave', e => {
+  //     this.hideCursor = true;
+  //   });
+  //   document.addEventListener('mouseenter', e => {
+  //     this.hideCursor = false;
+  //   });
 
-  },
+  // },
   // destroyed() {
   //   window.removeEventListener('mousemove', this.mouseIsMoving);
-  // },
+  },
   methods: {
-    moveCursor(e) {
-      this.xChild = e.clientX;
-      this.yChild = e.clientY;
-      setTimeout(() => {
-        this.xParent = e.clientX - 5;
-        this.yParent = e.clientY - 5;
-      }, 100);
-    },
-    mouseIsMoving(e){
-      // let x = e.pageX;
-      // let y = e.pageY;
-      // let xMouse = -300,
-      // yMouse = -300,
-      // lastScrolledLeft = 0,
-      // lastScrolledTop = 0;
-      // console.log(x, y, xMouse, yMouse);
-    },
+    // moveCursor(e) {
+    //   this.xChild = e.clientX;
+    //   this.yChild = e.clientY;
+    //   setTimeout(() => {
+    //     this.xParent = e.clientX - 5;
+    //     this.yParent = e.clientY - 5;
+    //   }, 100);
+    // },
+    // mouseIsMoving(e){
+    //   // let x = e.pageX;
+    //   // let y = e.pageY;
+    //   // let xMouse = -300,
+    //   // yMouse = -300,
+    //   // lastScrolledLeft = 0,
+    //   // lastScrolledTop = 0;
+    //   // console.log(x, y, xMouse, yMouse);
     onCalcIntersect(observer){
       if (observer.isIntersecting) {
         setTimeout(() => {
@@ -213,73 +211,73 @@ export default {
           this.mapShow = observer.isIntersecting
         }, 1000);
       }
-    },
+    }
   }
 }
 </script>
 <style lang="scss">
 @import '~/assets/_variables.scss';
-body, html {
-  width: 100%;
-  height: 100%;
-  // background: #000;
-  margin: 0;
-  cursor: none;
-  // display: flex;
-  // justify-content: center;
-  // align-items:  center;
-}
-.g-cursor {
-  &_hide {
-    opacity: 0;
-    width: 60px;
-    height: 60px;
-    transition: width .6s ease,
-      height .6s ease,
-      opacity .6s ease;
-  }
+// body, html {
+//   width: 100%;
+//   height: 100%;
+//   // background: #000;
+//   margin: 0;
+//   cursor: none;
+//   // display: flex;
+//   // justify-content: center;
+//   // align-items:  center;
+// }
+// .g-cursor {
+//   &_hide {
+//     opacity: 0;
+//     width: 60px;
+//     height: 60px;
+//     transition: width .6s ease,
+//       height .6s ease,
+//       opacity .6s ease;
+//   }
 
-  &__circle {
-    pointer-events: none;
-    user-select: none;
-    top: 0;
-    left: 0;
-    position: fixed;
-    width: 50px;
-    height: 50px;
-    border: 2px solid gray;
-    border-radius: 100%;
-    z-index: 5555;
-    backface-visibility: hidden;
-    transition: opacity .6s ease;
-  }
+//   &__circle {
+//     pointer-events: none;
+//     user-select: none;
+//     top: 0;
+//     left: 0;
+//     position: fixed;
+//     width: 50px;
+//     height: 50px;
+//     border: 2px solid gray;
+//     border-radius: 100%;
+//     z-index: 5555;
+//     backface-visibility: hidden;
+//     transition: opacity .6s ease;
+//   }
 
-  &__point {
-    top: 0;
-    left: 0;
-    position: fixed;
-    width: 10px;
-    height: 10px;
-    pointer-events: none;
-    user-select: none;
-    border-radius: 100%;
-    background: transparent;
-    z-index: 55555555;
-    backface-visibility: hidden;
-    will-change: transform;
-  }
+//   &__point {
+//     top: 0;
+//     left: 0;
+//     position: fixed;
+//     width: 10px;
+//     height: 10px;
+//     pointer-events: none;
+//     user-select: none;
+//     border-radius: 100%;
+//     background: transparent;
+//     z-index: 55555555;
+//     backface-visibility: hidden;
+//     will-change: transform;
+//   }
 
-  &_hover {
-    .g-cursor__circle {
-      opacity: 0;
-      width: 60px;
-      height: 60px;
-      transition: width .6s ease,
-        height .6s ease,
-        opacity .6s ease;
-    }
-  }
-}
+//   &_hover {
+//     .g-cursor__circle {
+//       opacity: 0;
+//       width: 60px;
+//       height: 60px;
+//       transition: width .6s ease,
+//         height .6s ease,
+//         opacity .6s ease;
+//     }
+//   }
+// }
 .main-block {
   & transition {
     transition: opacity 2s;
