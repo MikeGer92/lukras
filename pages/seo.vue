625a0338f474ef1e42de04b2a1e15bc4d94e9a4b
unknown <mikeman@inbox.ru>
2022-09-20 17:23:41 +0300

    added some blocks for seo-page

--- pages/seo.vue
+++ pages/seo.vue
@@ -1,7 +1,150 @@
 <template>
-  <div class="seo">
+  <section class="seo">
+    <div class="seo__banner"></div>
+    <div class="container">
+      <section class="seo__about">
+        <h1 class="seo__title">
+          <div class="seo__title-about">Септик "Тверь". Продажа и установка "под ключ"</div>
+        </h1>
+        <div class="seo__text">
+          <p class="seo__text-paragr">Септики просто незаменимы в тех случаях, когда подключение центральной канализационной системы невозможно.
+            «Тверь» - это универсальный септик. В зависимости от модели станция глубокой очистки может быть использована как на дачном участке,
+            так и в торговых центрах. Сама система представляет собой конструкцию из нескольких камер. После поступления стоков в камеры, внутри
+            них происходит процесс поэтапной очистки механическим и биологическим путём. Ассортимент септиков "Тверь" очень богат и включает в
+            себя 44 модели. Поэтому подобрать установку, подходящую по объему и производительности, совсем не сложно.
+          </p>
+          <div class="seo__divider"></div>
+          <ul class="seo__text-list">Каждая модель септика "Тверь" имеет свою маркировку, состоящую из литеры и приставки. Основные литеры:
+            <li>
+              <div class="seo__text-list_el">
+                <div class="seo__text-list_el-sqr">П</div>
+                <p class="seo__text-list_el-descr">— самотечные станции, изготовленные из полипропилена. Модели оборудованы армированными поясами
+                  и "пригрузочными юбками" для дополнительной надёжности установки. Рекомендованная глубина для отвода патрубка - не более 30 см.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="seo__text-list_el">
+                <div class="seo__text-list_el-sqr">М</div>
+                <p class="seo__text-list_el-descr">— септические установки "Тверь", предназначенные для более глубокой врезки
+                  патрубка (до 60 см.).
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="seo__text-list_el">
+                <div class="seo__text-list_el-sqr">Н</div>
+                <p class="seo__text-list_el-descr">— литера, обозначающая наличие насосных камер для очищения стоков. Высота входной трубки
+                  варьируется от 110 до 140 см. Отличное решение для установки септика под ключ на участках со сложным рельефом.
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="seo__image">
+          <div class="seo__image-about"><img src="@/assets/images/seo-constr.png"></div>
+        </div>
+      </section>
+      <section class="seo__constr">
+        <div class="seo__title">
+          <div class="seo__title-constr">Конструкция и механизм действия септика "Тверь"</div>
+        </div>
+        <div class="seo__divider"></div>
+        <div class="seo__text">
+          <ul class="seo__text-list">
+            <li class="seo__text-list_el">
+              <div class="seo__text-list_el-subtitle">Отстойник</div>
+              <p class="seo__text-list_el-info">Является неким фильтром, задерживающим тяжёлые нерастворимые взвеси, которые
+                оседают на дне отсека. Остальные лёгкие фракции отправляются в следующую камеру.
+              </p>
+            </li>
+            <li class="seo__text-list_el">
+              <div class="seo__text-list_el-subtitle">Анаэробный биореактор</div>
+              <p class="seo__text-list_el-info">Предназначен для механической очистки стоков, которая осуществляется за счет специальных
+                 фильтрационных ершей.
+              </p>
+            </li>
+            <li class="seo__text-list_el">
+              <div class="seo__text-list_el-subtitle">Аэротенк</div>
+              <p class="seo__text-list_el-info">Отсек, в который попадают остатки твердых веществ из стоковых вод. Расщепление
+                производится благодаря воздействию кислорода. В этом отсеке содержится повышенное число анаэробных бактерий,
+                что приводит к образованию ила.
+              </p>
+            </li>
+            <li class="seo__text-list_el">
+              <div class="seo__text-list_el-subtitle">Вторичный отстойник</div>
+              <p class="seo__text-list_el-info">Камера, обеспечивающая следующий этап очистки стоков. Тяжелые ферменты и частицы ила,
+                попадая во вторичный отстойник, продолжают разлагаться. Осветленные стоки попадают в следующий отсек, а оставшиеся
+                нерастворимые взвеси вновь оседают на дно и отправляются на повторную очистку в аэротенк.
+              </p>
+            </li>
+            <li class="seo__text-list_el">
+              <div class="seo__text-list_el-subtitle">Вторичный анаэробный биореактор</div>
+              <p class="seo__text-list_el-info">Осветленные стоки, попадающие в эту камеру, подвергаются повторной обработке микроорганизмами,
+                находящимися на специальных ершах.
+              </p>
+            </li>
+            <li class="seo__text-list_el">
+              <div class="seo__text-list_el-subtitle">Третичный отстойник</div>
+              <p class="seo__text-list_el-info">Последний этап очистки стоков. Соединение фосфатов с ионами магния и кальция позволяет
+                добиться максимальной очистки сточных вод (до 98%), а содержание реагентов хлора избавляет стоки от вредоносных бактерий.
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="seo__image">
+          <div class="seo__image-constr"><img src="@/assets/images/seo-meh.png"></div>
+        </div>
+      </section>
+      <section class="seo__mechanic">
+        <div class="seo__title">
+          <div class="seo__title-mechanic">Механизм работы септика "Тверь"</div>
+        </div>
+        <div class="seo__text">
+          <ul class="seo__text-list">Септик "Тверь" функционирует как и большинство классических очистных систем. Принцип работы подразделяется
+            на три основных этапа:
+            <li>
+              <div class="seo__text-list_el">
+                 <div class="seo__text-list_el-wrapper">
+                  <div class="seo__text-list_el-sqr">1</div>
+                  <div class="seo__text-list_el-subtitle">Разделение стоков на тяжёлые и лёгкие фракции.</div>
+                </div>
+                <p class="seo__text-list_el-descr">Осуществляется, как правило, в отсеках отстойниках.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="seo__text-list_el">
+                <div class="seo__text-list_el-wrapper">
+                  <div class="seo__text-list_el-sqr">2</div>
+                  <div class="seo__text-list_el-subtitle">Процесс брожения и очистки в результате взаимодействия с микроорганизмами.</div>
+                </div>
+                <p class="seo__text-list_el-descr">Для этих целей предназначен анаэробный биореактор.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="seo__text-list_el">
+                <div class="seo__text-list_el-wrapper">
+                  <div class="seo__text-list_el-sqr">3</div>
+                  <div class="seo__text-list_el-subtitle">Биологическая очистка.</div>
+                </div>
+                <p class="seo__text-list_el-descr">Максимальное очищение и обеззараживание стоковых вод достигается путем подачи кислорода,
+                  а также участия в процессе фосфатов, ионов кальция и магния, и реагентов хлора.
+                </p>
+              </div>
+            </li>
+          </ul>
+          <p class="seo__text-paragr">В моделях септиков "Тверь" без использования насосных систем перемещение стоков между камерами
+            достигается за счёт специально оборудованных систем переливов.
+          </p>
+        </div>
+      </section>
 
-  </div>
+
+    </div>
+
+  </section>
 </template>
 
 <script>
@@ -15,16 +158,131 @@
     mounted() {
       console.log(this.$route.path)
     }
-
   }
 </script>
 
 <style lang="scss">
-  .seo {
-    margin-top: 36px;
+.seo {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1600px;
+  margin: 0 auto;
+  &__banner {
+    margin: 36px 0 16px 0;
     width: 100%;
     min-height: 540px;
     background: url('@/assets/images/seo-banner.svg') 0px 0px/cover no-repeat;
   }
+  & .container {
+    max-width: 810px;
+  }
+  &__title {
+    margin: 48px 0 24px 0;
+    font-family: 'Roboto Condensed';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 64px;
+    line-height: 72px;
+    text-transform: uppercase;
+    color: #000000;
+    &-about, &-constr, &-mechanic {
+      font-family: 'Roboto Condensed';
+    }
+  }
+  &__text {
+    margin-top: 24px;
+    font-family: 'Roboto Serif';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 28px;
+    letter-spacing: 0.02em;
+    color: #3F3F3F;
+    &-list {
+      margin-top: 24px;
+      &_el {
+        display: flex;
+        margin-top: 24px;
+        &-sqr {
+          width: 28px;
+          height: 28px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin-right: 16px;
+          border: 1px solid #000000;
+          flex: 0 0 auto;
+        }
+      }
+    }
+  }
+  &__divider {
+    width: 100%;
+    height: 1px;
+    margin: 24px 0;
+    background: #E3E3E3;
+  }
+  &__image {
+    width: 100%;
+    max-width: 810px;
+    margin: 48px 0;
+    padding: 10px;
+    border: 1px solid #E3E3E3;
+    &-about {
+      display: flex;
+      width: 100%;
+      object-fit: contain;
+      & img {
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
+  &__constr {
+    .seo__text {
+      &-list {
+        &_el {
+          flex-direction: column;
+          padding-bottom: 24px;
+          border-bottom: 1px solid #E3E3E3;
+          &-subtitle {
+            font-weight: 600;
+            margin: 24px 0 16px 0;
+            &:first-child {
+              margin: 0 0 16px 0;
+            }
+          }
+        }
+      }
+    }
+  }
+  &__mechanic {
+    .seo__text {
+      &-list {
+        &_el {
+          flex-direction: column;
+          padding-bottom: 24px;
+          border-bottom: 1px solid #E3E3E3;
+          &-wrapper {
+            display: flex;
+            margin: 0px 0 16px 0;
+            align-items: center;
+          }
+          &-subtitle {
+            font-weight: 600;
+          }
+          &-descr {
+            padding-left: 46px;
+          }
+        }
+      }
+      &-paragr {
+        margin-top: 24px;
+      }
+
+    }
+  }
+}
 
 </style>
